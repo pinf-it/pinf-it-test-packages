c4a955116b4f1fa4f4d64c436b02d2f4ccc82d37
cadorn <christoph@christophdorn.com>
2013-02-20 14:56:43 -0800

    display version

--- helloworld-node-connect/main.js
+++ helloworld-node-connect/main.js
@@ -1,6 +1,18 @@
 
+const PATH = require("path");
+const FS = require("fs");
+
+
 exports.connect = function(req, res, next) {
+	var version = JSON.parse(FS.readFileSync(PATH.join(__dirname, "../package.json"))).version;
+    res.end("hello world from " + version);
+}
 
-    res.end("hello world\n");
 
+if (require.main === module) {
+	exports.connect(null, {
+		end: function(msg) {
+			console.log(msg);
+		}
+	}, null);
 }
